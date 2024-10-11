1e1da13b4719d70294a6fd7c635e824bcab44135
Alon Trachtenberg <alon402@gmail.com>
2024-10-11 23:49:17 +0300

    Update Static.cpp

--- 21_static/Static.cpp
+++ 21_static/Static.cpp
@@ -8,7 +8,15 @@ static int s_var = 5; // cannot be referenced in other cpp files by extern. s_va
 //each translation will have its own private s_var member during compilation
 
 
-//static isgood for encapsulation, so that the linker won't pick up duplications across translation units
+/*
+ static function -> scope limit to current translation unit by applying internal linkage
+ Without static, a function has a external linkage,--> translation units (other .cpp files in the project)
+ could access the function. If two or more files define functions with the same name and do not use static,
+ the linker will encounter multiple definitions and raise an error.
+
+ Note that inline functions do not apply internal linkage. inline does not violate ODR - same inline function can be
+ defined in multiple files - possible because the code itself is simply copied, no real function will exist at compile-time.
+*/
 static void function()
 {
 
