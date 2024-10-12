24c34ef7422e868c5440745f54e0a2c441385e63
Alon Trachtenberg <alon402@gmail.com>
2024-10-12 13:40:43 +0300

    Update car.h

--- 22b_class_header/car.h
+++ 22b_class_header/car.h
@@ -1,6 +1,7 @@
 #pragma once
 
 /*
+Reminder:
 headers contains forward declerations - all cpp files that want to use Car 
 class can include this header file. forward declerations satisfy the compile -
 "the definitions exists somewhere", during linkage the forward declerations will
@@ -8,13 +9,12 @@ be linked with the single source file that contains the defintions - saves binar
 instead of multiple files including full-fledged definitions, multiple forward declarations
 will point to a single defintion file.
 
-Notes:
-1. User defined types (such as classes) are exempt from ODR (one defintion rule - 1
-definition across all the program). multiple different source files (cpp) can potentially include 
-a header file that has fully defined variables and functions. This inclusion will lead 
-each cpp file to have a class with fully defined functions during compilation. In other words, a single
-cpp cannot have multiple redifnitions of the same class, but the same class can be defined in multiple cpp's
-(the definitions can be identical or different across files).
+The question:
+Why is it possible to potentially include a fully defined class in a header (functions can
+be defined in the header or in a separate cpp) in multiple cpp files?
+User defined types (such as classes) are exempt from ODR (one defintion rule - 1
+definition across all the program). In other words, a single cpp cannot have multiple 
+redifnitions of the same class. (Although a single cpp file cannot have the same class redifined multiple times).
 
 This is possbile because:
 
@@ -39,12 +39,13 @@ Including fully defined header is problematic for 2 reasons:
 	which includes full defintions for each function of the class. 
 
 	2. each small change in the fully-defined header causes the need for recompiling
-	all the files that make use of the class. instead, when using a header with forward declarations
-	and a single defined cpp file,  we can simply recompile the cpp definition of the class
-	(recompilation of a single translation unit), and relink to create exe.
+	all the files that make use of the class. 
 
+instead, when using a header with forward declarations only and a single defined cpp file,  
+we can simply recompile the cpp definition of the class (recompilation of a single translation unit),
+and relink to create exe.
 
-Advantages for using header files:
+Advantages for using header files with forwad declarations only:
 
 	1. less bloat and better performance - it is better to separate the class into a header file
 	that includes forward declerations (satisfies the compiler - "the definition exists somewhere"
@@ -56,6 +57,8 @@ Advantages for using header files:
 	single pass even when only forward declarations exist. the linker will later link them up
 	with their definition. This faster then "multi-pass" compilers such as java and c# (they 
 	are more convinient - no header files).
+ 	Technically, C# does not have a traditional linker. Instead, it compiles to an intermediate language (IL),
+  	which is then JIT-compiled at runtime by the Common Language Runtime (CLR).
 
 	3. Separate interface from implementation - 
 	headers make it easier to understand the class because they don't include implementation details,
