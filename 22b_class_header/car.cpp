a0ba0342fc2fda19c645707d4d1480832c38e872
Alon Trachtenberg <alon402@gmail.com>
2024-10-12 14:05:20 +0300

    Update car.cpp

--- 22b_class_header/car.cpp
+++ 22b_class_header/car.cpp
@@ -6,21 +6,24 @@ Notes:
 implementation of functions in the following format: return type, name and parameters.
 keywords such , override, virtual, static, __declspec(dllimport) 
 (49_static_dynamic_linking), should only be used in the header, where the functions
-are declared, and discarded in .cpp (implementation).
-when dealing with a class, the class should be declared in the header, it should
-contain the fields and function declarations. the functions will be implemented
-in the cpp
+are declared
+Why?
+Header file == class interafce ==  function signatures and behaviors (whether they are virtual, static, etc.). 
+It tells the compiler and anyone using the class what they can expect in terms of functionality.
+By marking a function as virtual in the header file, you inform users of the class and the compiler that
+this function is intended to be overridden in derived classes (interaction).
+The cpp on the other hand contains implementation details.
 
 
-C++ Standard n3337 ง 7.1.2/5 says:
+C++ Standard n3337 ยง 7.1.2/5 says:
 The virtual specifier shall be used only in the initial declaration of a non-static class member function;
 **************************************************************************
 
-2. //int Car::m_speed = 4; error!
-	non-static members such as m_speed (private or public) can only be defined using "default member initialization"
-	inside the class or via ctor - they can not be defined outside the class in a cpp file like static members.
-	check 36_member_initializer_list to see the construction phase and what the compiler chooses to initialize
-	the variable with (the value at the inline definition or the value supplied to the ctor)
+2. cpp should not include class variable member definition //int Car::m_speed = 4; error!
+non-static variable members such as m_speed (private or public) can only be defined using "default member initialization"
+inside the class (in the header) - they can not be defined outside the class in a cpp file like static members.
+check 36_member_initializer_list to see the construction phase and what the compiler chooses to initialize
+the variable with (the value at the inline definition or the value supplied to the ctor)
 
 */
 
@@ -34,13 +37,11 @@ int Car::m_wheel_amount = 4;
 
 Car::Car()
 {
-	m_speed = 0;
+	m_speed = 0; // possible to initialize members in a ctor
 }
 
-//with namespaces we can utilize "using" to avoid repeating the  namespace name
-//when implementing functions from the .h file in .cpp file, or wrapping the whole
-//implementation inside a namespace with the same name of the namespace from the header file. But! When 
 //implementing class functions there is no "using", There is no way to avoid the "Car::" qualification!
+// like done with "using std" and then we can execute cout<<x, no "using" here.
 void Car::accelerate(float intensity)
 {
 	m_speed += ACCELERATION_FACTOR * intensity;
