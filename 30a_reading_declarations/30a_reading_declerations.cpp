202114b980308a77e7d5ab09b72794d46520dc11
Alon Trachtenberg <alon402@gmail.com>
2024-10-18 16:13:27 +0300

    Update 30a_reading_declerations.cpp

--- 30a_reading_declarations/30a_reading_declerations.cpp
+++ 30a_reading_declarations/30a_reading_declerations.cpp
@@ -1,6 +1,7 @@
 /*
 based on: http://unixwiz.net/techtips/reading-cdecl.html
 http://users.ece.utexas.edu/~ryerraballi/CPrimer/CDeclPrimer.html
+https://cdecl.org/
 
 syntax of c decleration is of the form:
 storage-class	type	qualifier	declarator	=	initializer;
@@ -29,11 +30,11 @@ long** foo[7];
 /*
 1. (rule 1) start with the identifier: name is a.... returned type, e.g: foo is a......... long
 from now on we only use rule 2
-2. going right, because we must:    foo is "array of"..... long
+2. going right, because we can:    foo is "array of 7 long
 3. going left, because we must:		foo is array of 7 "pointer to"...long
-4. going left, because we must:		foo is array of 7 pointer to "pointer to" long.
-in other words, foo is an array which contains 7 pointer to pointers to long
-
+4. going left, because we must:		foo is array of 7 pointers to long pointers.
+in other words, foo is an array of 7 pointers to long pointers (i.e each long pointer points to a long pointer.
+the latter pointer can point to a single long, or a consequtive longs.
 
 ******remember that parenthesis have the highest priority
 int &arr[4] - arr is an array of 4 reference to an int (impossible to store references in a container)
@@ -70,25 +71,23 @@ name should be in:
 examples:
 for the abstract decleration: char*[]  -> (unamed is an array of char pointers) ->  (with identifier: char* x[])
 for the abstract decleration: void(*)() -> ( (unnamed) pointer to a function returning void)  -> with identifier: void(*p)()
-	void(*(int, int, int))(int, int, int) -> (unamed) is a function (int, int,int) returning a pointer to a function
-	(int,int,int) that returns a void -> with identifier: void(*p(int, int, int))(int, int, int) 
+
 */
 
 
 /*
-Note: the rhs is NOT A DECLARATION , but it is an expression
-that is evaluated into a single value
+Note: the rhs here is NOT A DECLARATION , but it is an expression that is evaluated into a single value:
  int offset_A=*(int*)(*(int*)pcv+4)
  therefore the rules above doesn't Exactly apply for reading expressions, for example
  in declarations * means "pointer to" but in expressions it means dereferncing
- But in general we can use the rules for navigation:
+ Aside from that,  we can use the same rules for navigation:
 
- 1. start with variable name (if exists) - pcv in this case
+1. start with variable name (if exists) - pcv in this case
 2. go right when you can, go left when you must
 
  pcv is an address that is cast to a pointer to an int (the compiler will treat that location as
  if it stores an int), we add 4 (the compiler deduces 4 as 16 bits (4*4) because we add
- 4 to a pointer to an int) and then this address is dereferenced, casted to an int
+ 4 to a pointer to an int) and then this address is dereferenced, casted to an int pointer
  and then dereferenced again
 
 */
