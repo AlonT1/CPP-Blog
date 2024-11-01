7e26e318aebaab617f039fa25291ca0c5e7da27a
Alon Trachtenberg <alon402@gmail.com>
2024-11-01 13:41:26 +0200

    Update 67_unions.cpp

--- 67_unions/67_unions.cpp
+++ 67_unions/67_unions.cpp
@@ -1,74 +1,97 @@
-/*
-Unions - only occupy memory of 1 member of a time. if we have a struct with 4 floats
-its size would be 4*4 = 16 bytes. a union can only have 1 member. so if we declare a union
-with 4 floats a,b,c,d, the union's size will be 4. and when we try to change
-the value of a,b,c or d we will change the value of all since they share the same
-memory block. the size of the union is the size of the largest member.
-in c, unions only work for pod's.
-
-unions are used in type punning. view (66_type_punning). espsecially when we like two different names
-the same variable. e.g: math vec3(X,Y,Z) can also be used for a color (R,G,B).
-used annonymously without names
-*/
 #include <iostream>
 
-struct Vec2
-{
-	float t, m;
-};
-
-struct Vec4
-{
-	union
-	{
-		struct
-		{
-			float x, y, z, w;
-		};
-		struct
-		{
-			Vec2 a, b;
-			// a represents t,m which aligns with x,y (in the struct above)
-			// b represents another instance of t,m that aligns with z,w (in the struct above)
-		};
-	};
-};
-
-std::ostream& operator<< (std::ostream& stream, Vec2 vec2)
-{
-	return stream << vec2.t << " " << vec2.m;
-}
 
 int main()
 {
-	std::cout << "Hello World!\n";
-
-	// a & b occupy the same memory block, its size is the size of the biggest type,
-	//which is float in this uniion (float is bigger then int)
-	struct Union
+	// ***************Given a Union:
+	union Data // union is a keyword used to defined a user-defined type "Data"
 	{
-		union // the union
+		int i;
+		float f;
+	};
+	/* The union occupies the size of the biggest member (float) as potentially all members can utilize this space (one at a time).
+	The member that is actually stored in the allocated memory is the last assigned member*/
+
+	union Data data; // instantiating Data type. "union" keyword mandatory in C, not in CPP (see "struct")
+	data.i = 10; // data union now stores 10 (int) in the allocated memory. data is now INTERPRETED as int.
+	data.f = 22.5f; // data union now stores 22.5 in the allocated memory. data is now INTERPRETED as  float.
+	// as data is now a float and both data.i and data.f occupy the same memory, data.i prints garbage value (1102315520)
+	// (since 10 is interpreted as a float)
+	std::cout << data.i << '\n'; 
+	
+
+	// ****************unions can be annonymous just like a struct and be stored in a struct
+	struct MyUnion
+	{
+		union // annonymous union
 		{
 			float a; 
 			int b;
 		};
 	};
 
-	Union u;
+	MyUnion u;
 	u.a = 2.0f; //now the union struct is dominated by float! accessing u.a gives correct result!
-	std::cout << u.a << " " << u.b << std::endl; //result: 2 and 1073741824 
-	//since 1073741824 is a byte representation as a float, in other words,
-	//we treate this "float memory block" as if it was an int.
-	//both a and b share the same memory block
 
-	u.b = 3; //now the union struct is dominated by int! accessing u.b gives correct result!
-	std::cout << u.a << " " << u.b << std::endl; //result: 4.2039e and 2
+	// ******************scoped non annonymous union
+	struct MyUnion1
+	{
+		union NamedUnion // annonymous union
+		{
+			float a; 
+			int b;
+		};
+	};
+
+	MyUnion1::NamedUnion nameUnion; // scoped to MyUnion1, but not related to a MyUnion instance
+	u.a = 2.0f; //now the union struct is dominated by float! accessing u.a gives correct result!
+
+	// ************************with instance
+	struct MyUnion2
+	{
+		union NamedUnion // annonymous union
+		{
+			float a; 
+			int b;
+		}nameUnion; // instance of NamedUnion
+	};
+
+	MyUnion2 myUnion2;
+	myUnion2.namedUnion = 5;
+
+
+	// *******************union with multiple structs
+	struct Vec2 
+	{
+    		float x, y;
+	};
+	
+	struct TheUnion
+	{
+		union // annonymous union
+		{
+			struct // annonymous struct
+			{
+				float x, y, z, w;
+			};
+			struct
+			{
+				Vec2 a, b; // "a" stores 2 floats and "b' stores 2 floats
+			};
+		};
+	};
+
+	TheUnion theUnion; // size of Vec2 (as it is the biggest member)
+	// "vec2 a" (stores 2 floats) is aligned with "float x,y" so the following will effect xy (x=5, y=6)
+	theUnion.a = {5.0f, 6.0f};
+	// "vec2 b" (stores 2 floats) is aligned with "float z,w" so the following will effect zw (zw z=5, w=6)
+	theUnion.b = {3.0f, 2.0f};
+	
 
 	/*---------------------------------------------------------
 	in effect the union struct simulates a type of any value!!!
 	similar to std::variant or std::any
-	The union struct can be either a float or an int, exclusively!!!
-	and can "change" time on-the-fly.
+	The union struct can be INTERPRETED either a float or an int on the fly, exclusively!!!
 	u.a = 3.0f; -> now the union represents a float (u.b gives undefined result
 	BECAUSE THE SINGLE MEMORY BLOCK STORES A FLOAT VALUE IN A FLOAT SIZED BLOCK)
 
@@ -77,8 +100,4 @@ int main()
 	this works becuase the compiler knows to read up to 4 bytes (int) of memory out of the union's memory block
 	(the size of the block is determined by the biggest union member)
 	------------------------------------------------------------*/
-
-	Vec4 v4 = { 1.0f, 2.0f, 3.0f, 4.0f };
-	std::cout << v4.a << " " << v4.b << std::endl;
-
 }
